752f67caaeaad12e69c7957e6b27b8889220419b
adam deeley <ad.deeley@gmail.com>
2017-01-09 20:53:36 +0000

    Added move_counter() method to Board.

--- draughts/draughts.py
+++ draughts/draughts.py
@@ -1,41 +1,53 @@
 class Board():
-
+    ''' Sets up a blank draughts board'''
     def  __init__(self):
         rows = "ABCDEFGH"
         cols = "12345678"
-        tileNames = [x+y for x in rows for y in cols]
-        self.tiles = dict([x, None] for x in tileNames )
+        self.tileNames = [x+y for x in rows for y in cols]
+        self.tiles = dict([x, None] for x in self.tileNames )
         assert len(self.tiles) == 64
-        
-    def setup_board(self):
-        '''Sets up a draughts board with the pieces in the starting positions
-        - returns dictionary board with references A1 to H8
-        - to add'''
-        rows = "ABCDEFGH"
-        cols = "12345678"
-        tileNames = [x+y for x in rows for y in cols]
-        tiles = dict([x, None] for x in tileNames )
-        assert len(tiles) == 64
-        return tiles
     
     def place_counters(self):
         ''' Places the counters on the board for red and black.
             - returns nothing '''
         starting_spaces = ['A2', 'A4', 'A6', 'A8', 'B1', 'B3', 'B5', 'B7', 
-        'C1', 'C3', 'C5', 'C7', 'F1', 'F3', 'F5', 'F7',  
+        'C2', 'C4', 'C6', 'C8', 'F1', 'F3', 'F5', 'F7',  
         'G2', 'G4', 'G6', 'G8', 'H1', 'H3', 'H5', 'H7']
         
         for tile in starting_spaces[0:12]:
-            self.tiles[tile] = Counter(tile, "Red")
+            self.tiles[tile] = Counter(tile, "R")
     
         for tile in starting_spaces[12:]:
-            self.tiles[tile] = Counter(tile, "Black")
+            self.tiles[tile] = Counter(tile, "B")
             
-    def get_tile(self, tile):
-        return self.tiles[tile]
-        
-class Counter():
+    def display_board(self):
+        counter = 0
+        for tile in self.tileNames:
+            if counter %8 == 0:
+                print("\n")
+            if self.tiles[tile] != None:
+                print(self.tiles[tile].get_colour(), end=", ")
+            else:
+            
+                print(".", end=", ")
+            counter += 1
     
+    def get_tile(self, tile):
+        if self.tiles[tile]:
+            return self.tiles[tile]
+        else:
+            return None
+        
+    def move_counter(self, source, destination):
+        ''' Moves the counter if the destination tile is free. '''
+        if self.tiles[source] and not self.tiles[destination]:
+            temp = self.tiles[source]
+            self.tiles[source] = None
+            self.tiles[destination] = temp
+        else:
+            print("Invalid move!")
+        
+class Counter():    
     def __init__(self,  location, colour):
         self.location = location
         self.colour = colour
@@ -47,3 +59,12 @@ class Counter():
     def  get_location(self):
         ''' Returns a string of the location of the counter e.g. "A1" '''
         return self.location
+
+        
+class Player1():    
+    def __init__(self):
+        pass
+        
+class Player1():    
+    def __init__(self):
+        pass
--- draughts/draughts_test.py
+++ draughts/draughts_test.py
@@ -5,8 +5,8 @@ class test_board_methods(unittest.TestCase):
 
     def test_setup_board(self):
         b = Board()
-        board = b.setup_board()
-        self.assertEqual(len(board),  64)
+        board = b.place_counters()
+        self.assertEqual(len(b.tiles),  64)
 
     def test_get_tile(self):
         b = Board()
@@ -17,21 +17,30 @@ class test_board_methods(unittest.TestCase):
         b = Board()
         b.place_counters()
         starting_spaces = ['A2', 'A4', 'A6', 'A8', 'B1', 'B3', 'B5', 'B7', 
-        'C1', 'C3', 'C5', 'C7', 'F1', 'F3', 'F5', 'F7',  
+        'C2', 'C4', 'C6', 'C8', 'F1', 'F3', 'F5', 'F7',  
         'G2', 'G4', 'G6', 'G8', 'H1', 'H3', 'H5', 'H7']
         for space in starting_spaces[0:12]:
             self.assertEqual(b.tiles[space] .get_location(), space)
-            self.assertEqual(b.tiles[space].get_colour(), "Red")
+            self.assertEqual(b.tiles[space].get_colour(), "R")
         
         for space in starting_spaces[12:]:
             self.assertEqual(b.tiles[space] .get_location(), space)
-            self.assertEqual(b.tiles[space].get_colour(), "Black")
+            self.assertEqual(b.tiles[space].get_colour(), "B")
+
+    def test_move_counter(self):     
+        b = Board()
+        b.place_counters()
+        b.move_counter("C2", "D3")
+        self.assertEqual(b.get_tile("C2"), None)
+        self.assertNotEqual(b.get_tile("D3"), None)
+        
+        
             
 class test_counter_methods(unittest.TestCase):
 
     def test_counter(self):
-        counter = Counter("A1", "Red")     
-        self.assertEqual(counter.get_colour(),  "Red")
+        counter = Counter("A1", "R")     
+        self.assertEqual(counter.get_colour(),  "R")
         self.assertEqual(counter.get_location(), "A1")
         
 if __name__ == '__main__':
